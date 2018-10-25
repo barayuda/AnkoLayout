4368f1c4babbf94bf86795f00b528edfe571898b
Barayuda Gautama <saya.bara@gmail.com>
2018-10-25 14:42:33 +0800

    Explore Anko Component

--- app/src/main/java/web/barayuda/ankolayout/MainActivity.kt
+++ app/src/main/java/web/barayuda/ankolayout/MainActivity.kt
@@ -1,8 +1,12 @@
 package web.barayuda.ankolayout
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,11 +24,74 @@ class MainActivity : AppCompatActivity() {
                             margin = dip(15)
                         }
 
-                val name = editText() {
+                val name = editText {
                     hint = "What's your name?"
                 }
 
-                button("Say Hello")
+                button("Say Hello") {
+                    backgroundColor = ContextCompat.getColor(context, R.color.colorAccent)
+                    textColor = Color.WHITE
+                    onClick { toast("Hello, ${name.text}!") }
+                }.lparams(width = matchParent){
+                    topMargin = dip(5)
+                }
+
+                button("Show Alert"){
+                    backgroundColor = ContextCompat.getColor(context, R.color.colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        alert("Happy Coding!", "Hello, ${name.text}") {
+                            yesButton { toast("Oh") }
+                            noButton { toast("Noo") }
+                        }.show()
+                    }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
+
+                button("Show Selector") {
+                    backgroundColor = ContextCompat.getColor(context, R.color.colorAccent)
+                    textColor = Color.WHITE
+
+                    onClick {
+                        val hero = listOf("Alucard", "Miya", "Lesley", "Chou")
+                        selector("Hello, ${name.text}! Apa hero favorite di Mobile Legends?", hero) {
+                            _, i -> toast("Jadi hero favoritmu ${hero[i]}?")
+                        }
+                    }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
+
+                button("Show Snackbar") {
+                    backgroundColor = ContextCompat.getColor(context, R.color.colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        snackbar(name, "Hello, ${name.text}!")
+                    }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
+
+                button("Show ProgressBar") {
+                    backgroundColor = ContextCompat.getColor(context, R.color.colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        indeterminateProgressDialog("Hello, ${name.text}! Mohon tunggu...").show()
+                    }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
+
+                button("Go to Second Activity") {
+                    backgroundColor = ContextCompat.getColor(context, R.color.colorAccent)
+                    textColor = Color.WHITE
+                    onClick {
+                        startActivity<SecondActivity>("name" to "${name.text}")
+                    }
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
             }
         }
     }
